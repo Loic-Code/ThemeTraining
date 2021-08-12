23ec212f76d15d114ae986265fa9ffe58ff12774
bouboumael <bouboumael@orange.fr>
2021-08-12 08:50:07 +0200

    alt

--- page-about.php
+++ page-about.php
@@ -23,7 +23,7 @@ Template Post Type: page
                             $size = 'thumbnail';
                         ?>
                         <div class="card col-12 col-md-7 col-lg-5 col-xl-3 mt-2 mb-5 shadow me-xl-1">
-                            <img src="<?= $photo[$params][$size] ?>" class="card-img-top mt-4 border" alt="<?= $photo['alt'] ?>">
+                            <img src="<?= $photo[$params][$size] ?>" class="card-img-top mt-4 border" alt="<?= $photo['alt']=== '' ? 'photo de ' . esc_html($personnel['nom']) : $photo['alt'] ?>">
                             <div class="card-body">
                                 <h5 class="card-title"> <?= esc_html(ucfirst($personnel['nom'])) ?> </h5>
                                 <p class="card-text"> <?= esc_html(ucfirst($personnel['description'])) ?> </p>
