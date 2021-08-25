15e83a40ffa0e569bba7718b46d93e6e1d601e8e
bouboumael <bouboumael@orange.fr>
2021-08-25 17:19:41 +0200

    debug

--- parts/homepage_staff.php
+++ parts/homepage_staff.php
@@ -8,33 +8,39 @@ if (!isset($personnels)) {
     <?php if ($personnels) : ?>
         <div class="row d-flex justify-content-around">
             <?php $i = 1;
-            foreach ($personnels as $personnel) : ?>
+            foreach ($personnels as $acfFieldName => $personnel) : ?>
                 <?php if ($i <= 3) : ?>
-                    <?php if ($personnel['nom'] !== '') : ?>
-                        <?php
-                        /*
-                         * params picture
-                         */
-                        $photo = $personnel['photo'];
-                        $params = 'sizes';
-                        $size = 'thumbnail';
+                    <?php if ($acfFieldName !== 'description'): ?>
+                        <?php if ($personnel['nom'] !== '') : ?>
+                            <?php
+                            /*
+                             * params picture
+                             */
+                            $photo = $personnel['photo'];
+                            $params = 'sizes';
+                            $size = 'thumbnail';
 
-                        ?>
-                        <figure class="profile mb-5" data-aos="fade-up" data-aos-duration="1000">
-                            <div class="profile-image"><img src="<?= $photo[$params][$size] ?>" alt="staff" data-aos="fade" data-aos-duration="2000" />
-                                <div class="icons">
-                                    <a class="mx-4" href="<?= $personnel['facebook'] ?>"><i class="ion-social-facebook"></i></a>
-                                    <a class="mx-4" href="<?= $personnel['twitter'] ?>"> <i class="ion-social-twitter"></i></a>
-                                    <a class="mx-4" href="<?= $personnel['instagram'] ?>"> <i class="ion-social-instagram"></i></a>
+                            ?>
+                            <figure class="profile mb-5" data-aos="fade-up" data-aos-duration="1000">
+                                <div class="profile-image"><img src="<?= $photo[$params][$size] ?>" alt="staff"
+                                                                data-aos="fade" data-aos-duration="2000"/>
+                                    <div class="icons">
+                                        <a class="mx-4" href="<?= $personnel['facebook'] ?>"><i
+                                                    class="ion-social-facebook"></i></a>
+                                        <a class="mx-4" href="<?= $personnel['twitter'] ?>"> <i
+                                                    class="ion-social-twitter"></i></a>
+                                        <a class="mx-4" href="<?= $personnel['instagram'] ?>"> <i
+                                                    class="ion-social-instagram"></i></a>
+                                    </div>
                                 </div>
-                            </div>
-                            <figcaption>
-                                <h3><?= esc_html(ucfirst($personnel['nom'])) ?></h3>
-                                <h4>Public Relations</h4>
-                                <p><?= esc_html(ucfirst($personnel['description'])) ?></p>
-                            </figcaption>
-                            <div class="banner-staff"></div>
-                        </figure>
+                                <figcaption>
+                                    <h3><?= esc_html(ucfirst($personnel['nom'])) ?></h3>
+                                    <h4>Public Relations</h4>
+                                    <p><?= esc_html(ucfirst($personnel['description'])) ?></p>
+                                </figcaption>
+                                <div class="banner-staff"></div>
+                            </figure>
+                        <?php endif; ?>
                     <?php endif; ?>
                     <?php $i++; ?>
                 <?php endif; ?>
@@ -42,6 +48,7 @@ if (!isset($personnels)) {
         </div>
     <?php endif; ?>
     <div class="d-flex justify-content-center">
-        <a href="<?php the_permalink() ?>" class="btn btn--with-icon" data-aos="fade" data-aos-duration="1000"><i class="fas fa-arrow-right"></i>VOIR LE STAFF</a>
+        <a href="<?php the_permalink() ?>" class="btn btn--with-icon" data-aos="fade" data-aos-duration="1000"><i
+                    class="fas fa-arrow-right"></i>VOIR LE STAFF</a>
     </div>
 </main>
