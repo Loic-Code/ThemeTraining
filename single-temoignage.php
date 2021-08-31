8958971b11a171fcef2c7b07a82e916c031e8e17
RedPandore <tennessee-houry_student2021@wilder.school>
2021-08-31 09:31:56 +0200

    add photo on testimony

--- single-temoignage.php
+++ single-temoignage.php
@@ -29,7 +29,7 @@ get_header();
             <figure class="testimony mt-3 mx-auto" data-aos="fade-up" data-aos-duration="1000">
                 <blockquote class="d-flex align-items-center"><span class="mx-auto text-center fst-italic"><?= $row['message'] ?></span></blockquote>
                 <div class="author">
-                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sq-sample1.jpg" alt="sq-sample1" />
+                    <img src="<?= $row['photo'] ?>" alt="sq-sample1" />
                     <h5><?= $row['nom'] ?><span><?= $row['titre_du_message'] ?></span></h5>
                 </div>
             </figure>
